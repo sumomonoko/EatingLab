9a41eb523818e5a565a37c0a7137bd04b17b018b
shiori saitou <shi.o.rin.5082@gmail.com>
2024-04-20 12:14:01 +0000

    [Add]ジャンル検索,いいね機能,コメント機能

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,14 +1,14 @@
 class Public::CommentsController < ApplicationController
   def create
-    food = Food.find(params[:food_id])
+    @food = Food.find(params[:food_id])
     food_comment = current_user.comments.new(comment_params)
-    food_comment.food_id = food.id
+    food_comment.food_id = @food.id
     food_comment.save
-    redirect_to request.referer
   end
 
   def destroy
     food_comment = Comment.find(params[:id])
+    @food = food_comment.food
     food_comment.destroy
   end
 
--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -2,5 +2,6 @@ class Public::SearchesController < ApplicationController
   def genre_search
     @genre_id = params[:genre_id]
     @foods = Food.where(genre_id: @genre_id)
+    @genres = Genre.all
   end
 end
