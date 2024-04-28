1690b2ada20f8e9a8b23b0b5a106ed050587bb88
shiori saitou <shi.o.rin.5082@gmail.com>
2024-04-28 05:46:20 +0000

    [add]dm

--- app/controllers/public/dms_controller.rb
+++ app/controllers/public/dms_controller.rb
@@ -2,15 +2,15 @@ class Public::DmsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    room = Room.find(params[:id])
-    message = current_user.messages.new(dm_params)
-    message.save
-    redirect_to user_room_path(room)
+    room = Room.find(params[:room_id])
+    current_user.dms.create(dm_params)
+    redirect_to user_room_path(room.dm_partner(current_user), room)
   end
 
   private
 
   def dm_params
-    params.require(:dm).permit(:message, :room_id, :user_id)
+    params.permit(:message, :room_id, :user_id)
+    
   end
 end
